858348b98f3775e83f2b1baf4548fa21779e2fc9
joan.sarola <joan.sarola@somenergia.coop>
2023-12-03 00:32:05 +0100

    Changed for passing a parameter everyday to execute a different module

--- src/main.rs
+++ src/main.rs
@@ -1,133 +1,39 @@
-use std::fs::File;
-use std::io::{BufReader, BufRead, Error};
-use std::alloc;
-use regex::Regex;
+mod day1;
+mod day2;
 
+use std::env;
 
-fn main() -> Result<(), Error> {
-    let path = "day1/input-test-part-1.txt";
-    // let path = "day1/input.txt";
+fn main() {
+    // Get the command-line arguments
+    let args: Vec<String> = env::args().collect();
 
-    let input = File::open(path)?;
-    let buffered = BufReader::new(input);
-
-    let re = Regex::new(r"[0-9]{1}").unwrap();
-    let hay = "aasd23ss111b227c";
-
-    let mat = re.find(hay).unwrap();
-
-    let mut suma: i32 = 0;
-
-    for line in buffered.lines() {
-        println!("{:?}", line);
-
-        let cadena = line.unwrap(); 
-        
-        let mut numeros: Vec<&str> = re.find_iter(&cadena).map(|m| m.as_str()).collect();
-        
-        println!("{:?}", numeros);
-
-
-    // println!("Firt element {:?}: ", numeros.pop());
-
-        let primer_caracter = numeros[0].parse::<i32>();
-        let ultim_caracter = numeros[numeros.len()-1].parse::<i32>();
-
-        let mut combi = [numeros[0],numeros[numeros.len()-1]].join("").parse::<i32>();
-
-//        println!("{:?}", primer_caracter);
- //       println!("{:?}", ultim_caracter);
-  //      println!("{:?}", combi);
-
-        suma = &suma + &combi.unwrap();
+    // Check if at least one argument is provided
+    if args.len() < 2 {
+        println!("Usage: {} <function_name>", args[0]);
+        return;
     }
-    println!("{:?}", suma);
 
-    //    hay.find(&re)
-//    for line in buffered.lines() {
-//        let cadena = line?;
-//        println!("{}", cadena);
-//        
-        // let first_digit = cadena.find(&re);
-//    }
-let re = Regex::new(r"[0-9]{1}|eight|one|two|three|four|five|six|seven|nine").unwrap();
+    // Get the function name from the command-line argument
+    let function_name = &args[1];
 
-let mut sum_calibration: i32 = 0;
-let path = "day1/input-test-part-2.txt";
-//let path = "day1/input.txt";
-
-let input = File::open(path)?;
-let buffered = BufReader::new(input);
-
-fn change_number(numero: &str) -> &str {
-    if numero == "one" { "1" }
-    else if numero == "two" { "2" }
-    else if numero == "three" { "3" }
-    else if numero == "four" { "4" }
-    else if numero == "five" { "5" }
-    else if numero == "six" { "6" }
-    else if numero == "seven" { "7" }
-    else if numero == "eight" { "8" }
-    else if numero == "nine" { "9" }
-    else { numero }
-}
-
-for line in buffered.lines() {
-    println!("{:?}", line);
-
-    // let cadena = line.unwrap(); 
-    
-    let mut cadena_buscar: String = line.unwrap();
-
-    let mut numeros = Vec::new();
-
-    
-
-    loop {
-        println!("1st Buscar {:?}", cadena_buscar);
-        let element = re.find(&cadena_buscar).unwrap();
-
-        if ! element.is_empty() { 
-            println!("Element trobat {:?}", element.as_str());
-            numeros.push(element.as_str());
-
-            let inici = element.start()+1;
-
-            /*
-            string té un problema perquè fa servir punters com C, i per tant no "matxaca" la posició
-            de memòria. 
-            */
-            let _cadena_nova: String = cadena_buscar.drain(inici..).collect();
-            
-
-            //println!("Nova {:?}", cadena_nova);
-            //let cadena_buscar = &cadena_nova.to_string();
-            println!("Buscar {:?}", cadena_buscar);
-           
-        } else {
-            break;
+    // Execute the function based on the argument
+    match function_name.as_str() {
+        "day1" => say_day1(),
+        "day2" => say_day2(),
+        _ => {
+            println!("Unknown function: {}", function_name);
+            println!("Available functions: say_hello, say_goodbye");
         }
     }
-
-    // let mut numeros: Vec<&str> = re.find_iter(&cadena).map(|m| m.as_str()).collect();
-    
-    println!("{:?}", numeros);
-
-    let primer_caracter = change_number(numeros[0]);
-    let ultim_caracter = change_number(numeros[numeros.len()-1]);
-
-    let mut combi = [primer_caracter,ultim_caracter].join("").parse::<i32>();
-
-    println!("{:?}", primer_caracter);
-    println!("{:?}", ultim_caracter);
-    println!("{:?}", combi);
-
-    sum_calibration = &sum_calibration + &combi.unwrap();
 }
 
-println!("{:?}", sum_calibration);
+fn say_day1() {
+    day1::exec_day1();
+}
 
-
-    Ok(())
-
+fn say_day2() {
+    day2::exec_day2();
+}
+fn say_goodbye() {
+    println!("Goodbye, world!");
 }
